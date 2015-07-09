1a57a17a4c9eaf052fc86a81271e635fef4e7776
amitgude <gudeamit148@gmail.com>
2015-07-09 16:35:26 -0700

    version 3

--- OnlineTest1/src/BallotButton.java
+++ OnlineTest1/src/BallotButton.java
@@ -1,9 +1,11 @@
 
 public class BallotButton implements Button {
-
+   
+	private boolean clickStatus = false;
 	@Override
 	public Boolean click() {
 		// TODO Auto-generated method stub
+		clickStatus = true;
 		return true;
 	}
 
--- OnlineTest1/src/ControlUnit.java
+++ OnlineTest1/src/ControlUnit.java
@@ -4,8 +4,8 @@ public class ControlUnit {
 	Button ballotButton = new BallotButton();
 	Button totalButton = new TotalButton();
 	Lamp onLamp = new OnLamp();
-	Lamp busyLamp = new OnLamp();
-	
+	Lamp busyLamp = new BusyLamp();
+	Constituency con = new Constituency("Ponda");
 	private boolean onStatus;
 	
 	
@@ -16,12 +16,30 @@ public class ControlUnit {
 	public void powerOnLamp(){
 		
 		if (this.onStatus == true){
-			
+			onLamp.lighton();
+		}
+		else{
+			onLamp.lightoff();
 		}
 	}
 	
 	public void clickBallotButton(){
 		
+		if (ballotButton.click()){
+			
+		}
+	}
+
+	
+	
+	
+	public void addCandidate(Candidate c){
+		con.addCandidate(c);
+	}
+
+	public void getCandidates() {
+		// TODO Auto-generated method stub
+		con.getCandidates();
 	}
 	
 	
